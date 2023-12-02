c82b6de39f5fb729fa8bb3b56070caf6a4dd460f
Agatha Niwinski <agathaniw@gmail.com>
2023-12-02 14:24:13 -0500

    Adding CSS Changes

--- themes/twentytwenyone-child/functions.php
+++ themes/twentytwenyone-child/functions.php
@@ -9,9 +9,10 @@
  * @since Twenty Twenty-One-Child 1.0
  */
 
-add_action( 'wp_enqueue_scripts', 'twentytwentyonechild_wp_enqueue_styles' );
 function twentytwentyonechild_wp_enqueue_styles() {
-	wp_enqueue_style( 'main', get_stylesheet_uri().'/assets/css/main.css', array( 'twenty-twenty-one-style', 'twenty-twenty-one-print-style', 'bootstrap' ), wp_get_theme()->get( 'Version' )	);
-	wp_enqueue_style( 'bootstrap', get_stylesheet_directory_uri().'/assets/css/vendor/bootstrap.css', array( ), '5.3.2' );
+	wp_enqueue_style( 'main', get_stylesheet_directory_uri() .'/assets/css/main.css', array( 'twenty-twenty-one-style', 'twenty-twenty-one-print-style', 'bootstrap' ), wp_get_theme()->get( 'Version' )	);
+	wp_enqueue_style( 'bootstrap', get_stylesheet_directory_uri() .'/assets/css/vendor/bootstrap.css', array( ), '5.3.2' );
 
 }
+
+add_action( 'wp_enqueue_scripts', 'twentytwentyonechild_wp_enqueue_styles' );
